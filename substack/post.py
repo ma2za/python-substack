5d65771b84e71cdd4db63d6e486985b8584bfa2e
mazza8 <59370937+mazza8@users.noreply.github.com>
2023-01-10 17:22:17 +0100

    Develop (#4)
    * additional methods
    
    * subscribe button
    
    * add youtube support
    
    * bump patch version

--- substack/post.py
+++ substack/post.py
@@ -12,11 +12,21 @@ class Post:
 	def add(self, item):
 		self.draft_body["content"] = self.draft_body.get("content", []) + [{"type": item.get("type")}]
 		content = item.get("content")
-		if content is not None:
-			self.text(content)
+		if item.get("type") == "captionedImage":
+			self.captioned_image(item)
+		elif item.get("type") == "youtube2":
+			self.youtube(item.get("src"))
+		else:
+			if content is not None:
+				self.add_complex_text(content)
 
 		if item.get("type") == "heading":
 			self.attrs(item.get("level", 1))
+
+		marks = item.get("marks")
+		if marks is not None:
+			self.marks(marks)
+
 		return self
 
 	def paragraph(self, content=None):
@@ -41,6 +51,22 @@ class Post:
 		self.draft_body["content"][-1]["attrs"] = content_attrs
 		return self
 
+	def captioned_image(self, value):
+		content = self.draft_body["content"][-1].get("content", [])
+		content += [{"type": "image2", "attrs": {
+			"src": value.get("src"),
+			"fullscreen": False,
+			"imageSize": value.get("size", "normal"),
+			"height": 819,
+			"width": 1456,
+			"resizeWidth": 728,
+
+			"bytes": None, "alt": None, "title": None, "type": None, "href": None, "belowTheFold": False,
+			"internalRedirect": None
+
+		}}]
+		self.draft_body["content"][-1]["content"] = content
+
 	def text(self, value: str):
 		"""
 
@@ -57,11 +83,25 @@ class Post:
 		self.draft_body["content"][-1]["content"] = content
 		return self
 
+	def add_complex_text(self, text):
+		if isinstance(text, str):
+			self.text(text)
+		else:
+			for chunk in text:
+				if chunk:
+					self.text(chunk.get("content")).marks(chunk.get("marks"))
+
 	def marks(self, marks):
 		content = self.draft_body["content"][-1].get("content", [])[-1]
 		content_marks = content.get("marks", [])
 		for mark in marks:
-			content_marks.append({"type": mark})
+			new_mark = {"type": mark.get("type")}
+			if mark.get("type") == "link":
+				href = mark.get("href")
+				new_mark.update({"attrs": {
+					"href": href
+				}})
+			content_marks.append(new_mark)
 		content["marks"] = content_marks
 		return self
 
@@ -72,3 +112,23 @@ class Post:
 		out = vars(self)
 		out["draft_body"] = json.dumps(out["draft_body"])
 		return out
+
+	def subscribe_with_caption(self, value):
+		content = self.draft_body["content"][-1].get("content", [])
+		content += [{"type": "subscribeWidget",
+		             "attrs": {"url": "%%checkout_url%%", "text": "Subscribe"},
+		             "content": [
+			             {
+				             "type": "ctaCaption",
+				             "content": [{"type": "text",
+				                          "text": f"Thanks for reading {value}! Subscribe for free to receive new posts and support my work."}]
+			             }
+		             ]}]
+		self.draft_body["content"][-1]["content"] = content
+		return self
+
+	def youtube(self, value):
+		content_attrs = self.draft_body["content"][-1].get("attrs", {})
+		content_attrs.update({"videoId": value})
+		self.draft_body["content"][-1]["attrs"] = content_attrs
+		return self
